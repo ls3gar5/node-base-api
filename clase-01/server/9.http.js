1003050f5c4e6f9285aeda0a81e8d058b0941938
Leonardo <leonardo@finalis.com>
2025-05-08 09:21:16 -0300

    chore(clase-01): fix scaffolding folder

---
+++ clase-01/server/9.http.js
@@ -0,0 +1,20 @@
+const http = require('http')
+const findAvailablePort = require('./10.free-port.js')
+
+const port = process.env.PORT ?? 3000
+
+const server = http.createServer((req, res) => {
+  console.log('Request received:', req.method, req.url)
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.end('Hello, World!\n')
+})
+
+// server.listen(0, () => {
+//   console.log(`Server is running on http://localhost:3010', ${server.address().port}`)
+// })
+
+findAvailablePort(port).then(port => {
+  server.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
+  })
+})
