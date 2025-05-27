73234041f41f710f304fad4d08c3a571cf892915
Leonardo Segars <leonardosegars@Leonardos-MacBook-Pro.local>
2025-05-27 16:32:51 -0300

    fix():api key

--- food-trucks-map/src/app/app.component.ts
+++ food-trucks-map/src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule, NgFor } from '@angular/common';
-import { GoogleMapsModule, MapInfoWindow } from '@angular/google-maps';
+import { GoogleMapsModule } from '@angular/google-maps';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '@envs/environment'; // Ensure this import is correct based on your environment setup
 
 @Component({
   selector: 'app-root',
@@ -14,13 +15,13 @@ export class AppComponent {
   center: google.maps.LatLngLiteral = { lat: 37.77551013804947, lng: -122.39099930600248 };
   zoom = 14;
   foodTrucks: any[] = [];
-
+  
   constructor(private http: HttpClient) {
     this.loadFoodTrucks(this.center.lat, this.center.lng);
   }
 
   loadFoodTrucks(lat: number, lon: number) {
-    const url = `https://l90o7ash07.execute-api.us-east-1.amazonaws.com/api/v1/foodtrucks/bylocation?lat=${lat}&lon=${lon}`;
+    const url = `${environment.API_URL}?lat=${lat}&lon=${lon}`;
     this.http.get<any>(url).subscribe(response => {
       this.foodTrucks = response.food_trucks.map((truck: any) => ({
         position: { lat: truck.latitude, lng: truck.longitude },
@@ -29,4 +30,15 @@ export class AppComponent {
       }));
     });
   }
+
+  onMapClick(event: google.maps.MapMouseEvent) {
+  if (event.latLng) {
+    const lat = event.latLng.lat();
+    const lng = event.latLng.lng();
+    console.log('Map clicked at:', lat, lng);
+
+    this.center = { lat, lng }; // optionally re-center the map
+    this.loadFoodTrucks(lat, lng); // fetch new food truck data
+  }
+}
 }
---
+++ food-trucks-map/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    googleMapsApiKey: '',
+    API_URL: 'https://l90o7ash07.execute-api.us-east-1.amazonaws.com/api/v1/foodtrucks/bylocation',
+};
